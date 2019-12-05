9809dd821b3ee406535a450c9205a9f0436eebe5
Krystal Manke <krystal.manke@gmail.com>
2019-12-05 12:28:20 -0600

    removed old test code

--- humanin.py
+++ humanin.py
@@ -4,15 +4,6 @@ import os
 from optparse import OptionParser
 
 
-# fasta_1 = r"C:\Users\kryst\Desktop\School\Genomics and Bioinfo\Fasta files\C_variegatus_sequence.fasta"
-# fasta_2 = r"C:\Users\kryst\Desktop\School\Genomics and Bioinfo\Fasta files\E_macularius_sequence.fasta"
-# fasta_3 = r"C:\Users\kryst\Desktop\School\Genomics and Bioinfo\Fasta files\G_luii_sequence.fasta"
-
-# test_gracilis = r"gracilis.fasta"
-# test_human = r"homo_sapien.fasta"
-# test_chimp = r"pan_troglodytes.fasta"
-
-
 def get_sequence(seq_file_path):
     sequence = ""
     with open(seq_file_path, "r") as file_in:
@@ -73,18 +64,20 @@ def count_positions(text_in, text_dict, set_in):
 def read_proteins(seq_file):
     aa_set = set()
     pos_dict = dict()
+    line_count = 0
     for i in range(0, 25):  # 25: the number of amino acids in the peptide (plus stop codon)
         pos_dict[i] = dict()
     with open(seq_file, "r") as file_in:
         for i in file_in.readlines():
+            line_count += 1
             pos_dict, aa_set = count_positions(i.rstrip(), pos_dict, aa_set)
     pos_dict = add_aa(pos_dict, aa_set)
-    return pos_dict, aa_set
+    return pos_dict, aa_set, line_count
 
 # print(pos_dict_out)
 
 
-def get_counts(pos_dict, aa_set):
+def get_counts(pos_dict, aa_set, line_count):
     counts_dict = dict()
     for aa in aa_set:
         counts_dict.setdefault(aa, [])
@@ -92,14 +85,14 @@ def get_counts(pos_dict, aa_set):
         for aa in pos_dict[place].keys():
             counts_dict[aa].append(pos_dict[place][aa] + 1)
     counts_df = pd.DataFrame(counts_dict)
-    probs_df = counts_df.applymap(lambda x: (x/32) if x != 0 else x)  # 32: twice the number of sequences considered to find a consensus
+    probs_df = counts_df.applymap(lambda x: (x/(line_count * 2)) if x != 0 else x)  # 32: twice the number of sequences considered to find a consensus
     consen_dict = {"Sequence": probs_df.idxmax(1), "Probabilities": probs_df.max(1)}
     consen_df = pd.DataFrame(consen_dict)
     consensus = "".join(counts_df.idxmax(1).values)
     return probs_df, consen_df, consensus
 
 
-def find_prob(prob_df, text_in):
+def find_prob(prob_df, text_in, line_count):
     aa_list = list(prob_df.columns)
     text_probs = 1
     for index, acid in enumerate(text_in):
@@ -107,31 +100,31 @@ def find_prob(prob_df, text_in):
             prob = prob_df.at[index, acid]
             text_probs = text_probs * prob
         else:
-            prob = 1/32
+            prob = 1/(line_count * 2)
             text_probs = text_probs * prob
     return text_probs
 
 
-def find_humanin(sequence_in, prob_df):
+def find_humanin(sequence_in, prob_df, line_count):
     prob = 0
     likely_hum = ""
     sequence_in.replace("-", "*")
     for i in range(0, len(sequence_in) - 25):
         subseq = sequence_in[i:(i + 25)]
-        sub_prob = find_prob(prob_df, subseq)
+        sub_prob = find_prob(prob_df, subseq, line_count)
         if sub_prob > prob:
             prob = sub_prob
             likely_hum = subseq
     return prob, likely_hum
 
 
-def yield_results(frame_opt, file_in, output_file, prob_opt, prob_df, con_prob):
+def yield_results(frame_opt, file_in, output_file, prob_opt, prob_df, con_prob, line_count):
     seq = get_sequence(file_in)
     trans = get_translation(seq)
     seqs = read_translation(trans)
     if frame_opt == "All":
         for key, value in seqs.items():
-            prob, motif = find_humanin(value, prob_df)
+            prob, motif = find_humanin(value, prob_df, line_count)
             if prob_opt:
                 rel_prob = str((prob/con_prob) * 100) + "% "
             else:
@@ -142,7 +135,7 @@ def yield_results(frame_opt, file_in, output_file, prob_opt, prob_df, con_prob):
         max_prob = 0
         max_motif = ""
         for key, value in seqs.items():
-            prob, motif = find_humanin(value, prob_df)
+            prob, motif = find_humanin(value, prob_df, line_count)
             if prob > max_prob:
                 max_prob = prob
                 max_key = key
@@ -154,7 +147,7 @@ def yield_results(frame_opt, file_in, output_file, prob_opt, prob_df, con_prob):
         output_file.write(file_in.split("\\")[-1] + " - Frame: " + max_key + ": " + rel_prob + max_motif + "\n\n")
     else:
         for key, value in seqs.items():
-            prob, motif = find_humanin(value, prob_df)
+            prob, motif = find_humanin(value, prob_df, line_count)
             if prob_opt:
                 rel_prob = str((prob / con_prob) * 100) + "% "
             else:
@@ -167,12 +160,12 @@ def main():
     parser = OptionParser()
     parser.add_option("-i", "--input", dest="input", help="Read data from FASTA file; can also be a directory path if "
                                                             "the -d option is used")
-    parser.add_option("-o", "--output", dest="output", help="Name of the output file")
+    parser.add_option("-o", "--output", dest="output", help="Prefix of the output file")
     parser.add_option("-m", "--matrix", dest="matrix", default="humanin.txt", help="File containing the motif sequences "
                                                                                    "used to create the probability "
                                                                                    "matrix; each line should hold a "
                                                                                    "separate sequence; all sequences "
-                                                                                   "should be the same length")
+                                                                                   "should be the same length. Default file is humanin.txt")
     parser.add_option("-d", "--dir", action="store_true", dest="directory", default=False, help="Read data from FASTA "
                                                                                                 "files in this "
                                                                                                 "directory")
@@ -188,9 +181,9 @@ def main():
                                                                                                       "sequence)")
 
     (options, args) = parser.parse_args()
-    pos_dict_out, aa_set_out = read_proteins(options.matrix)
-    probability_df, consensus_df, consensus_text = get_counts(pos_dict_out, aa_set_out)
-    consensus_prob = find_prob(probability_df, consensus_text)
+    pos_dict_out, aa_set_out, line_count = read_proteins(options.matrix)
+    probability_df, consensus_df, consensus_text = get_counts(pos_dict_out, aa_set_out, line_count)
+    consensus_prob = find_prob(probability_df, consensus_text, line_count)
     # if len(args) < 1:
     #     parser.error("Input option required")
     if options.input:
@@ -201,57 +194,15 @@ def main():
                 for file in os.listdir(file_directory):
                     file_in = os.path.join(file_directory, file)
                     print("File: " + file_in)
-                    yield_results(options.frames, file_in, file_out, options.probabilities, probability_df, consensus_prob)
+                    yield_results(options.frames, file_in, file_out, options.probabilities, probability_df, consensus_prob, line_count)
         else:
             with open(options.output + "_humanin_results.txt", "w") as file_out:
                 file = options.input
-                yield_results(options.frames, file, file_out, options.probabilities, probability_df, consensus_prob)
+                yield_results(options.frames, file, file_out, options.probabilities, probability_df, consensus_prob, line_count)
     else:
         print("No input file specified.\nUse 'python humanin.py -h' for the help message.")
 
 
 if __name__ == "__main__":
     main()
-
-
-# test files
-
-# seq_1 = get_sequence(test_human)
-# trans_1 = get_translation(seq_1)
-# seqs_1 = read_translation(trans_1)
-#
-# seq_2 = get_sequence(test_chimp)
-# trans_2 = get_translation(seq_2)
-# seqs_2 = read_translation(trans_2)
-#
-# seq_3 = get_sequence(test_gracilis)
-# trans_3 = get_translation(seq_3)
-# seqs_3 = read_translation(trans_3)
-#
-# for key, value in seqs_1.items():
-#     print("H sapiens - Frame " + key + ": ", find_humanin(value))
-#
-# for key, value in seqs_2.items():
-#     print("P troglodytes - Frame " + key + ": ", find_humanin(value))
-#
-# for key, value in seqs_3.items():
-#     print("D gracilis - Frame " + key + ": ", find_humanin(value))
-
-# file_directory = r"squamates"
-# with open("squamates_results.txt", "w") as file_out:
-#     for file in os.listdir(file_directory):
-#         seq = get_sequence(os.path.join(file_directory, file))
-#         trans = get_translation(seq)
-#         seqs = read_translation(trans)
-#         for key, value in seqs.items():
-#             prob, motif = find_humanin(value)
-#             file_out.write(file + " - Frame: " + key + ": " + str(prob) + "% " + motif + "\n")
-#         file_out.write("\n")
-
-
-
-
-
-
-
 
